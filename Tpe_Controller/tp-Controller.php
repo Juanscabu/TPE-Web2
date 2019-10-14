0738bed9e210150bf4e2183d4f31eb620db57018
Juan Scabuzzo <Juanscabu@gmail.com>
2019-10-14 18:16:46 -0300

    Modifico archivo

--- Tpe_Controller/tp-Controller.php
+++ Tpe_Controller/tp-Controller.php
@@ -1,15 +1,20 @@
 <?php
-include_once('models/task.model.php');
-include_once('views/task.view.php');
+include_once('Tpe_Model/tp-Model.php');
+include_once('Tpe_View/tp.View.php');
 
-include_once ('prueba.php');
+class TpeController {
 
-$productos = getProductos();
+    private $model;
+    private $view;
 
-foreach($productos as $producto) {
-    echo $producto->nombre;
+    public function __construct() {
+        $this->model = new TpeModel();
+        $this->view = new TpeView();
+    }
+
+    public function mostrarProductos() {
+        $productos = $this->model->getProductos();
+        $this->view->mostrarProductos($productos);
+    }
 }
-
-
-
 ?>
