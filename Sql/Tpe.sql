5548b5babf6ad974f4df1a913bbee408b95610d3
Juan Scabuzzo <Juanscabu@gmail.com>
2019-10-31 17:15:41 -0300

    Archivo modificado

--- Sql/Tpe.sql
+++ Sql/Tpe.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 31-10-2019 a las 21:03:03
+-- Tiempo de generación: 31-10-2019 a las 21:14:06
 -- Versión del servidor: 10.1.40-MariaDB
 -- Versión de PHP: 7.3.5
 
