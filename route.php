c8dfa9ad6f400b20079d752b2f0dc6a023ce5494
Juan Scabuzzo <Juanscabu@gmail.com>
2019-11-26 21:46:52 -0300

    agregando cosas

--- route.php
+++ route.php
@@ -1,7 +1,7 @@
 <?php
     require_once('Controllers/productos-Controller.php');
     require_once('Controllers/pedidos-Controller.php');
-    require_once('Controllers/login-Controller.php');
+    require_once('Controllers/usuarios-Controller.php');
     require_once('Controllers/home-Controller.php');
     require_once('Router.php');
 
@@ -13,22 +13,29 @@
 
     $r = new Router();
    
-    $r->addRoute("verifica", "POST", "LoginController", "verificaUsuario");
+    $r->addRoute("verifica", "POST", "UsuariosController", "verificaUsuario");
     $r->addRoute("enviarPedido", "GET", "PedidosController", "cargaPedidos");
     $r->addRoute("addProducto", "POST", "ProductosController", "cargaProductos");
-    $r->addRoute("logout", "GET", "LoginController", "logout");
+    $r->addRoute("logout", "GET", "UsuariosController", "logout");
+    $r->addRoute("registro", "GET", "HomeController", "mostrarRegistro");
+    $r->addRoute("registrar", "POST", "UsuariosController", "registrarUsuario");
     $r->addRoute("mostrarHome", "GET", "HomeController", "mostrarHome");
     $r->addRoute("login", "GET", "PedidosController", "mostrarPedidos");
     $r->addRoute("mostrarProductos", "GET", "ProductosController", "mostrarProductos");
     $r->addRoute("mostrarPedidos", "GET", "PedidosController", "mostrarPedidos");
     $r->addRoute("editarPedido/:ID", "GET", "PedidosController", "editarPedido");
-    $r->addRoute("pedidoEditado", "GET", "PedidosController", "pedidoEditado");
+    $r->addRoute("pedidoEditado", "POST", "PedidosController", "pedidoEditado");
     $r->addRoute("editarProducto/:ID", "GET", "ProductosController", "editarProducto");
     $r->addRoute("productoEditado", "GET", "ProductosController", "productoEditado");
     $r->addRoute("borrarProducto/:ID", "GET", "ProductosController", "borrarProducto");
     $r->addRoute("detallePedido/:ID", "GET", "PedidosController", "detallePedido");
     $r->addRoute("borrarPedido/:ID", "GET", "PedidosController", "borrarPedido");
     $r->addRoute("mostrarPedidosOrdenados", "GET", "PedidosController", "mostrarPedidosOrdenados");
+    $r->addRoute("verUsuarios", "GET", "UsuariosController", "mostrarUsuarios");
+    $r->addRoute("editarUsuario/:USUARIO", "GET", "UsuariosController", "editarUsuario");
+    $r->addRoute("borrarUsuario/:ID", "GET", "UsuariosController", "borrarUsuario");
+    $r->addRoute("usuarioEditado", "GET", "UsuariosController", "usuarioEditado");
+    $r->addRoute("borrarImagen/:ID", "GET", "PedidosController", "borrarImagen");
     
    
     $r->setDefaultRoute("HomeController", "mostrarHome");
